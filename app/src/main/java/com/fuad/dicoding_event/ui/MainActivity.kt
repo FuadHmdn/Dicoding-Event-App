352a9ee455615ff2e02a877398158fef71891ce0
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-10-12 22:00:54 +0700

    init commit

---
+++ app/src/main/java/com/fuad/dicoding_event/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.fuad.dicoding_event.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fuad.dicoding_event.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
