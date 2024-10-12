a740bc7955189f10b421f3eeceaca854a932e495
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-10-12 23:26:43 +0700

    style recycler view upcoming list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
 }
