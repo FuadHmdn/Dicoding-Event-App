483e19492f5fc7af62d106acd2b0193cd3bcbc9f
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-10-13 19:15:43 +0700

    styling upcoming fragment

--- app/src/main/java/com/fuad/dicoding_event/ui/upcomingfragment/UpcomingEventFragment.kt
+++ app/src/main/java/com/fuad/dicoding_event/ui/upcomingfragment/UpcomingEventFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fuad.dicoding_event.R
+import com.fuad.dicoding_event.data.ListEventsItem
 import com.fuad.dicoding_event.databinding.FragmentFinishedEventBinding
 import com.fuad.dicoding_event.databinding.FragmentUpcomingEventBinding
 import com.fuad.dicoding_event.ui.finishedfragment.FinishedViewModel
+import com.fuad.dicoding_event.ui.homefragment.EventFinishedAdapter
 
 class UpcomingEventFragment : Fragment() {
 
@@ -25,6 +29,62 @@ class UpcomingEventFragment : Fragment() {
         val view = binding.root
         return view
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvFinishedFragment.layoutManager = LinearLayoutManager(context)
+
+        viewModel.listEventItemFinished.observe(viewLifecycleOwner){ listEvent->
+            setAdapter(listEvent)
+        }
+
+        viewModel.searchEventItemFinished.observe(viewLifecycleOwner) { list ->
+            if (list.isNullOrEmpty()) {
+                viewModel.listEventItemFinished.observe(viewLifecycleOwner){ listEvent->
+                    setAdapter(listEvent)
+                }
+            } else {
+                setAdapter(list)
+            }
+        }
+
+        viewModel.isLoading.observe(viewLifecycleOwner){
+            showLoading(it)
+        }
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView.editText
+                .setOnEditorActionListener { textView, i, _ ->
+                    searchBar.setText(searchView.text)
+                    if (i == EditorInfo.IME_ACTION_SEARCH) {
+
+                        val query = textView.text.toString()
+                        viewModel.searchEvent(query)
+                        searchView.hide()
+                        true
+
+                    } else {
+                        false
+                    }
+                }
+        }
+    }
+
+    private fun setAdapter(event: List<ListEventsItem?>?) {
+        val adapter = UpcomingAdapter()
+        adapter.submitList(event)
+        binding.rvFinishedFragment.adapter = adapter
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBarFinishedFragment.visibility = View.VISIBLE
+        } else {
+            binding.progressBarFinishedFragment.visibility = View.GONE
+        }
     }
 
 }
