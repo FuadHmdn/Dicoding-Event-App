366238d7d2b58f559383ae2f5aba4e864c8131c5
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-10-15 14:34:14 +0700

    final

--- app/src/main/java/com/fuad/dicoding_event/data/ApiConfig.kt
+++ app/src/main/java/com/fuad/dicoding_event/data/ApiConfig.kt
@@ -1,15 +1,20 @@
 package com.fuad.dicoding_event.data
 
+import com.fuad.dicoding_event.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
-    companion object{
-        fun getApiService() : ApiService{
+    companion object {
+        fun getApiService(): ApiService {
             val httpLoggingInterceptor =
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+                } else {
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+                }
 
             val client = OkHttpClient.Builder()
                 .addInterceptor(httpLoggingInterceptor)
